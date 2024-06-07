c67c746160095f6d43e728e06410a4c9b938e501
LucasTavares <lulucas25@hotmail.com>
2024-06-07 01:25:24 -0300

    config index, routes and controller

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const path = require("path");
+const routes = require("./routes/routes")
+
 const app = express();
+const port = 3000;
 
-app.get('/home',  (req, res) => {
-  res.send('Hello World');
-});
 
-app.listen(port,() => console.console.log(`Servidor rodando em http://localhost:${port}`)
+app.set("view engine", "ejs")
+app.use(express.static(path.join(__dirname, "public")))
+app.use(routes)
+
+
+
+app.listen(port,() => 
+console.log(`Servidor rodando em http://localhost:${port}`)
 );
